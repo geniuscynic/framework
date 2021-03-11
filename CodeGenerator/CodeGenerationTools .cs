2400f141a07d8f8de2acef1a0ef6381dd662c703
geniuscynic <350810375@qq.com>
2021-03-11 22:33:29 +0800

    complexquery 完成， 差传入sql

---
+++ CodeGenerator/CodeGenerationTools .cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DoCare.Extension.DataBase;
+
+namespace CodeGenerator
+{
+    public class TableDefind
+    {
+        public string ColumnName { get; set; }
+        public string DataTypeName { get; set; }
+
+        public int ColumnOrder { get; set; }
+
+        public int Length { get; set; }
+
+        public int IsNullable { get; set; }
+
+        public int IsIdentity { get; set; }
+    }
+
+    public class CodeGenerationTools
+    {
+        private Dbclient client;
+        public CodeGenerationTools(string connectionString, string provider)
+        {
+            client = new Dbclient(connectionString, provider);
+        }
+
+        public string MsSql = @"select c.name as ColumnName,
+c.colorder as ColumnOrder,
+c.xtype as DataType,
+typ.name as  DataTypeName,
+c.Length, c.isnullable as IsNullable,
+COLUMNPROPERTY (c.id, c.name,  'IsIdentity ') as IsIdentity
+from dbo.syscolumns c 
+inner join dbo.sysobjects t
+on c.id = t.id
+inner join dbo.systypes typ on typ.xtype = c.xtype
+where OBJECTPROPERTY(t.id, N'IsUserTable') = 1
+and t.name='{0}' order by c.colorder;";
+
+    
+
+        public async Task<IEnumerable<TableDefind>> GetTableDefine(string tableName)
+        {
+            //return await client.GetConnection().QueryAsync<TableDefind>(string.Format(MsSql, tableName));
+            return  await client.SimpleQueryable<TableDefind>(MsSql).ExecuteQuery();
+        }
+
+       
+    }
+}
---
+++ CodeGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectStr = "Server=localhost;Database=blog;Trusted_Connection=True;MultipleActiveResultSets=true";
+            var provider = "MsSql";
+
+            var tool = new CodeGenerationTools(connectStr, provider);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
